b5b7dd845159c71ba0048a1337876327fa236a76
aayussraut <aayussraut.ar@gmail.com>
2023-08-04 16:48:54 +0545

    new socketConnection utils

--- client/src/utils/socketConnection.js
+++ client/src/utils/socketConnection.js
@@ -1,9 +1,11 @@
 import socketIOClient from "socket.io-client";
-
+import sailsIOClient from "sails.io.js";
 export const connectToSocket = () => {
   let io;
   io = sailsIOClient(socketIOClient);
+
   io.sails.url = "http://localhost:1337";
+
   io.socket.on("connect", function onConnect() {
     console.log("This socket is now connected to the Sails server!");
   });
