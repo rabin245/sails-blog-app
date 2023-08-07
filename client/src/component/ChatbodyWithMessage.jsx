bbf40261cf451e07ec02eb3636df62fcfbca2b5a
rabin245 <drarabin@gmail.com>
2023-08-07 14:36:24 +0545

    tinker some of the styles

--- client/src/component/ChatbodyWithMessage.jsx
+++ client/src/component/ChatbodyWithMessage.jsx
@@ -1,6 +1,6 @@
 export default function ChatbodyWithMessage() {
   return (
-    <div className="flex items-center justify-center w-4/5 bg-slate-800 border">
+    <div className="flex items-center justify-center w-4/5 bg-slate-800">
       <h1 className="text-2xl font-bold">
         Open Chats to see your conversations
       </h1>
