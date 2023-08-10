a11cf568ad12a553df531af385a9faadaf85131b
aayussraut <aayussraut.ar@gmail.com>
2023-08-10 14:30:00 +0545

    sepeate join and leave blog and add socket.off

---
+++ client/src/utils/blogs.js
@@ -0,0 +1,22 @@
+export const joinRoom = async (io) => {
+  console.log("joining room\n\n\n\n");
+  return new Promise((resolve) => {
+    io.socket.get(`/join-blog`, (body, JWR) => {
+      console.log("Sails responded with: ", body);
+      console.log("with headers: ", JWR.headers);
+      console.log("and with status code: ", JWR.statusCode);
+      resolve(body);
+    });
+  });
+};
+
+export const leaveRoom = async (io) => {
+  return new Promise((resolve) => {
+    io.socket.get(`/leave-blog`, (body, JWR) => {
+      console.log("Sails responded with: ", body);
+      console.log("with headers: ", JWR.headers);
+      console.log("and with status code: ", JWR.statusCode);
+      resolve(body);
+    });
+  });
+};
