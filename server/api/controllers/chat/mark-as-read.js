3bc6910b55a03fda70538b39a93c2f5e46328361
aayussraut <aayussraut.ar@gmail.com>
2023-08-14 08:30:47 +0545

    backend for msg unread feature

---
+++ server/api/controllers/chat/mark-as-read.js
@@ -0,0 +1,55 @@
+module.exports = {
+  friendlyName: "Mark as read",
+
+  description: "",
+
+  inputs: {
+    id: {
+      type: "number",
+      required: true,
+    },
+  },
+
+  exits: {},
+
+  fn: async function (inputs, exits) {
+    const token = this.req.session.authToken;
+    const { id: userId } = await sails.helpers.getUserFromToken(token);
+
+    console.log("inputs", inputs);
+    const { id: sender } = inputs;
+    // const sender = parseInt(this.req.query.sender);
+    // console.log("sender", sender);
+
+    const chats = await Chat.find({
+      where: {
+        sender,
+        receiver: userId,
+        readStatus: false,
+      },
+    });
+
+    console.log("chats", chats);
+
+    const chats2 = await Chat.find({
+      where: {
+        sender: userId,
+        receiver: sender,
+        readStatus: false,
+      },
+    });
+
+    console.log("chats2", chats2);
+
+    await Chat.update({
+      sender,
+      receiver: userId,
+      readStatus: false,
+    }).set({
+      readStatus: true,
+    });
+    return exits.success({
+      message: "success",
+    });
+  },
+};
