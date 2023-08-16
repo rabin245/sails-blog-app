d02a1ba0883f80aba710055ae7ee7f0285f23236
rabin245 <drarabin@gmail.com>
2023-08-16 14:54:57 +0545

    add SWR

--- client/src/utils/blogs.js
+++ client/src/utils/blogs.js
@@ -2,9 +2,9 @@ export const joinRoom = async (io) => {
   console.log("joining room\n\n\n\n");
   return new Promise((resolve) => {
     io.socket.get(`/join-blog`, (body, JWR) => {
-      console.log("Sails responded with: ", body);
-      console.log("with headers: ", JWR.headers);
-      console.log("and with status code: ", JWR.statusCode);
+      // console.log("Sails responded with: ", body);
+      // console.log("with headers: ", JWR.headers);
+      // console.log("and with status code: ", JWR.statusCode);
       resolve(body);
     });
   });
@@ -13,9 +13,9 @@ export const joinRoom = async (io) => {
 export const leaveRoom = async (io) => {
   return new Promise((resolve) => {
     io.socket.get(`/leave-blog`, (body, JWR) => {
-      console.log("Sails responded with: ", body);
-      console.log("with headers: ", JWR.headers);
-      console.log("and with status code: ", JWR.statusCode);
+      // console.log("Sails responded with: ", body);
+      // console.log("with headers: ", JWR.headers);
+      // console.log("and with status code: ", JWR.statusCode);
       resolve(body);
     });
   });
@@ -24,9 +24,9 @@ export const leaveRoom = async (io) => {
 export const joinSingleRoom = async (io, id) => {
   return new Promise((resolve) => {
     io.socket.get(`/join-blog/${id}`, (body, JWR) => {
-      console.log("Sails responded with: ", body);
-      console.log("with headers: ", JWR.headers);
-      console.log("and with status code: ", JWR.statusCode);
+      // console.log("Sails responded with: ", body);
+      // console.log("with headers: ", JWR.headers);
+      // console.log("and with status code: ", JWR.statusCode);
       resolve(body);
     });
   });
@@ -35,9 +35,9 @@ export const joinSingleRoom = async (io, id) => {
 export const leaveSingleRoom = async (io, id) => {
   return new Promise((resolve) => {
     io.socket.get(`/leave-blog/${id}`, (body, JWR) => {
-      console.log("Sails responded with: ", body);
-      console.log("with headers: ", JWR.headers);
-      console.log("and with status code: ", JWR.statusCode);
+      // console.log("Sails responded with: ", body);
+      // console.log("with headers: ", JWR.headers);
+      // console.log("and with status code: ", JWR.statusCode);
       resolve(body);
     });
   });
